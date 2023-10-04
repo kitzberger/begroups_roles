b36543849c518e46081338e33cd89d5d2d99b802
Philipp Kitzberger <coding@kitze.net>
2023-10-04 22:24:50 +0200

    Add explanation

--- ext_tables.php
+++ ext_tables.php
@@ -5,7 +5,11 @@ call_user_func(function () {
     $GLOBALS['TYPO3_CONF_VARS']['BE']['toolbarItems'][1472569541] =
         \IchHabRecht\BegroupsRoles\Backend\ToolbarItems\RoleSwitcher::class;
 
-    // Flip order of RoleSwitch and UserToolbarItem
+    // Flip order of RoleSwitch and UserToolbarItem!
+    // Explanation: EXT:backend uses multiple consecutive keys to register several items
+    // to be rendered in the backends upper toolbar, so the only way to squeeze a new item in
+    // is to change the key of one of these items.
+    // 1435433111 is the UserToolbarItem (user avatar with dropdown for 'settings' and 'logout')
     $GLOBALS['TYPO3_CONF_VARS']['BE']['toolbarItems'][1472569542] = $GLOBALS['TYPO3_CONF_VARS']['BE']['toolbarItems'][1435433111];
     unset($GLOBALS['TYPO3_CONF_VARS']['BE']['toolbarItems'][1435433111]);
 
